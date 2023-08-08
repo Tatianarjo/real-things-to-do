47bb650f1e3281c030b3396711de11b5f9b726bb
Tatiana Johnson <Tatianarjo@gmail.com>
2023-08-08 12:18:05 -0500

    added props and styles

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oops:)</Text>
-      <StatusBar style="auto" />
+     {/*Today's Tasks*/}
+     <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Things I Get To Do</Text>
+
+      <View style={styles.items}>
+        {/*This is where the tasks will go*/}
+        <Task text={"Task 1"} />
+        <Task text={"Task 2"} />
+      </View>
+
+     </View>
     </View>
   );
 }
@@ -13,8 +24,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F0FF42'
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+    
+sectionTitle: {
+  fontSize: 24,
+  fontWeight: "bold"
+},
+items: {
+  marginTop: 30
+}
 });
+
+//#ECF8F9
+//#7e1717
+//#e55807
+//#068da9
